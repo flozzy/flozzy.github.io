c8b545dbdbd933f74d43d5ad9123b2cdc45f3769
Florence Chukwuemeka <5608767+flozzy@users.noreply.github.com>
2022-05-03 21:03:05 +0200

    clickable
    added the function to see the products

---
+++ hapterashop/main.js
@@ -0,0 +1,133 @@
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+  function openNav1() {
+    document.getElementById("myNav1").style.display = "block";
+  }
+  
+  function closeNav1() {
+    document.getElementById("myNav1").style.display = "none";
+  }
+  function openNav2() {
+    document.getElementById("myNav2").style.display = "block";
+  }
+  
+  function closeNav2() {
+    document.getElementById("myNav2").style.display = "none";
+  }
+  function openNav3() {
+    document.getElementById("myNav3").style.display = "block";
+  }
+  
+  function closeNav3() {
+    document.getElementById("myNav3").style.display = "none";
+  }
+  function openNav4() {
+    document.getElementById("myNav4").style.display = "block";
+  }
+  
+  function closeNav4() {
+    document.getElementById("myNav4").style.display = "none";
+  }
+  function openNav5() {
+    document.getElementById("myNav5").style.display = "block";
+  }
+  
+  function closeNav5() {
+    document.getElementById("myNav5").style.display = "none";
+  }
+  function openNav6() {
+    document.getElementById("myNav6").style.display = "block";
+  }
+  
+  function closeNav6() {
+    document.getElementById("myNav6").style.display = "none";
+  }
+  function openNav7() {
+    document.getElementById("myNav7").style.display = "block";
+  }
+  
+  function closeNav7() {
+    document.getElementById("myNav7").style.display = "none";
+  }
+  function openNav8() {
+    document.getElementById("myNav8").style.display = "block";
+  }
+  
+  function closeNav8() {
+    document.getElementById("myNav8").style.display = "none";
+  }
+  function openNav9() {
+    document.getElementById("myNav9").style.display = "block";
+  }
+  
+  function closeNav9() {
+    document.getElementById("myNav9").style.display = "none";
+  }
+  function openNav10() {
+    document.getElementById("myNav10").style.display = "block";
+  }
+  
+  function closeNav10() {
+    document.getElementById("myNav10").style.display = "none";
+  }
+  function openNav11() {
+    document.getElementById("myNav11").style.display = "block";
+  }
+  
+  function closeNav11() {
+    document.getElementById("myNav11").style.display = "none";
+  }
+  function openNav12() {
+    document.getElementById("myNav12").style.display = "block";
+  }
+  
+  function closeNav12() {
+    document.getElementById("myNav12").style.display = "none";
+  }
+  function openNav13() {
+    document.getElementById("myNav13").style.display = "block";
+  }
+  
+  function closeNav13() {
+    document.getElementById("myNav13").style.display = "none";
+  }
+  function openNav14() {
+    document.getElementById("myNav14").style.display = "block";
+  }
+  
+  function closeNav14() {
+    document.getElementById("myNav14").style.display = "none";
+  }
+  function openNav15() {
+    document.getElementById("myNav15").style.display = "block";
+  }
+  
+  function closeNav15() {
+    document.getElementById("myNav15").style.display = "none";
+  }
+  function openNav16() {
+    document.getElementById("myNav16").style.display = "block";
+  }
+  
+  function closeNav16() {
+    document.getElementById("myNav16").style.display = "none";
+  }
+  function openNav17() {
+    document.getElementById("myNav17").style.display = "block";
+  }
+  
+  function closeNav17() {
+    document.getElementById("myNav17").style.display = "none";
+  }
+  function openNav18() {
+    document.getElementById("myNav18").style.display = "block";
+  }
+  
+  function closeNav18() {
+    document.getElementById("myNav18").style.display = "none";
+  }
